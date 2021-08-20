d7a46eb820ab191054711e2cca650a0ac2626639
Richard Warburton <richard.warburton@gmail.com>
2021-08-20 16:04:17 +0200

    Parse out the tezos block hash from the snapshot page

--- bench.sh
+++ bench.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-export DATE="$(date +'%d')"
+export DATE="-1"
+export SNAPSHOT_BLOCK=""
 export OPSIAN_OPTS="apiKey=$OPS_KEY,applicationVersion=1,debugLogPath=/data/£{ARGV_0}-£{PID}-debug.log,agentId=£{ARGV_0}-snapshot"
 
 while true
@@ -12,16 +13,20 @@ do
   # Download a new snapshot once per day
   NEW_DATE=$(date +'%d')
   if [ "$DATE" -ne "$NEW_DATE" ]
-    then rm -f /data/snapshot
+  then
+    SNAPSHOT_LINE=$(curl -s https://snapshots-tezos.giganode.io | grep 'href="https://snapshots-tezos.giganode.io/snapshot-mainnet' | head -n 1)
+    SNAPSHOT_URL=$(echo "$SNAPSHOT_LINE" | sed 's/.*href="//;s/".*//')
+    SNAPSHOT_BLOCK=$(echo "$SNAPSHOT_LINE" | sed 's/.*_//;s/\.rolling.*//')
+
+    rm -f /data/snapshot
+    wget "$SNAPSHOT_URL" -O /data/snapshot || true
   fi
   DATE=$NEW_DATE
 
-  SNAPSHOT_URL=$(curl -s https://snapshots-tezos.giganode.io | grep 'href="https://snapshots-tezos.giganode.io/snapshot-mainnet' | head -n 1 | sed 's/.*href="//;s/".*//')
-  wget -nc "$SNAPSHOT_URL"  -O /data/snapshot || true
 
   rm -rf /data/tezos-node-mainnet
   ./tezos-node config init --data-dir /data/tezos-node-mainnet --network mainnet
-  time ./tezos-node snapshot --data-dir /data/tezos-node-mainnet import --block BLzFs2hjX8YuoWkFL2VqLT4vHH2N3CruHcZ7gZG4ejbp2zCVPbE /data/snapshot
+  time ./tezos-node snapshot --data-dir /data/tezos-node-mainnet import --block "$SNAPSHOT_BLOCK" /data/snapshot
 
 done
 
