05822625b7d2cff50f2fd4738c7e7b72c96adceb
Richard Warburton <richard.warburton@gmail.com>
2021-10-19 17:25:38 +0100

    Don't redownload the snapshot on the first attempt if it already exists

--- bench.sh
+++ bench.sh
@@ -2,9 +2,15 @@
 
 set -eux
 
-export DATE="-1"
+export DATE=""
+DATE=$(date +'%d')
 export SNAPSHOT_BLOCK=""
 export OPSIAN_OPTS="apiKey=$OPS_KEY,applicationVersion=1,debugLogPath=/data/£{ARGV_0}-£{PID}-debug.log,agentId=£{ARGV_0}-snapshot"
+
+if [ -f /data/block ]
+then
+  SNAPSHOT_BLOCK=$(cat /data/block)
+fi
 
 while true
 do
@@ -12,17 +18,17 @@ do
 
   # Download a new snapshot once per day
   NEW_DATE=$(date +'%d')
-  if [ "$DATE" -ne "$NEW_DATE" ]
+  if [ "$DATE" -ne "$NEW_DATE" ] || [ ! -f /data/snapshot ]
   then
     SNAPSHOT_LINE=$(curl -s https://snapshots-tezos.giganode.io | grep 'href="https://snapshots-tezos.giganode.io/snapshots/mainnet' | head -n 1)
     SNAPSHOT_URL=$(echo "$SNAPSHOT_LINE" | sed 's/.*href="//;s/".*//')
     SNAPSHOT_BLOCK=$(echo "$SNAPSHOT_LINE" | sed 's/.*_//;s/\.rolling.*//')
 
     rm -f /data/snapshot
+    echo "$SNAPSHOT_BLOCK" > /data/block
     wget "$SNAPSHOT_URL" -O /data/snapshot || true
   fi
   DATE=$NEW_DATE
-
 
   rm -rf /data/tezos-node-mainnet
   ./tezos-node config init --data-dir /data/tezos-node-mainnet --network mainnet
