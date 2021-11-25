840baa82160655433fbb712eab53bf995d36f2b9
Richard Warburton <richard.warburton@gmail.com>
2021-11-25 16:18:42 +0100

    Add in run script

---
+++ run-tezos-bench.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+export OPS_KEY=$1
+export DATA_DIR=$2
+export CONT=$3
+
+docker run -it \
+  --cap-add=SYS_PTRACE \
+  -v "$DATA_DIR":/data \
+  -e OPS_KEY="$OPS_KEY" \
+  --add-host snapshots-tezos.giganode.io:20.79.209.6 "$CONT"
+
